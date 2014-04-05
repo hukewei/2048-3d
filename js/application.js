9e36e2084fcd1a9fe5724760fcf636b68cac7694
kewei <keweihu@yahoo.com>
2014-04-05 17:28:12 +0200

    add leapmotion support

--- js/application.js
+++ js/application.js
@@ -1,4 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(3, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+// Setup Leap loop with frame callback function
+  window.game = new GameManager(3, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
